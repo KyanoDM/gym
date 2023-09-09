b7540423dc213908288a6615a72c98adcaddce7a
Kyano DM <81612475+KyanoDM@users.noreply.github.com>
2023-09-09 20:56:01 +0200

    pssw

---
+++ docs/js/script.js
@@ -0,0 +1,28 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const overlay = document.getElementById("overlay");
+    const mainContent = document.getElementById("main-content");
+    const passwordInput = document.getElementById("password");
+    const loginButton = document.getElementById("login-button");
+
+    // Password to access the website
+    const correctPassword = "gym";
+
+    // Show the overlay
+    overlay.style.visibility = "visible";
+    overlay.style.opacity = 1;
+
+    // Handle login button click
+    loginButton.addEventListener("click", function () {
+        const enteredPassword = passwordInput.value;
+
+        if (enteredPassword === correctPassword) {
+            // Correct password, hide the overlay
+            overlay.style.visibility = "hidden";
+            overlay.style.opacity = 0;
+        } else {
+            // Incorrect password, display a message
+            alert("Incorrect password. Please try again.");
+            passwordInput.value = "";
+        }
+    });
+});
